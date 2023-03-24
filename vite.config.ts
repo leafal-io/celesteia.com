afc708f63300f9f8da7552fe1dc6cd5363067c81
Morgan <morgan@morganhofmann.nl>
2023-03-24 15:25:18 +0100

    Add linting

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import solid from "solid-start/vite";
-import { defineConfig } from "vite";
-import cloudflare from "solid-start-cloudflare-pages";
+import cloudflare from 'solid-start-cloudflare-pages';
+import solid from 'solid-start/vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [solid({ adapter: cloudflare({}) })],
+    plugins: [solid({ adapter: cloudflare({}) })],
 });
